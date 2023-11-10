6e78be6a2f247788c439dd6f5faa10766ffa6fbb
bawanesumit01 <bawanesumit01@gmail.com>
2023-11-10 15:59:13 +0530

    commit

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,11 +8,11 @@
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="5000">
                 <img src="assets/images/slider-img-1.webp"
-                    style="height:400px;" class="d-block w-100 banner_image" alt="..." data-stellar-background-ratio="0.5">
+                    style="height:400px;object-fit:cover;" class="d-block w-100 banner_image" alt="..." data-stellar-background-ratio="0.5">
             </div>
             <div class="carousel-item" data-bs-interval="3000">
                 <img src="assets/images/slider-img-2.webp"
-                    style="height:400px;" class="d-block w-100 banner_image" alt="..." data-stellar-background-ratio="0.5">
+                    style="height:400px;object-fit:cover;" class="d-block w-100 banner_image" alt="..." data-stellar-background-ratio="0.5">
             </div>
           
         </div>
